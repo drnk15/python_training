ba19d84469c1a1299cb69a045d4ab26697d2e12a
natalia <konv.42@gmail.com>
2021-11-18 22:54:13 +0300

    conftest created, test_del_group added

---
+++ test/test_del_group.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+def test_delete_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.open_groups_page()
+    app.group.delete_first_group()
+    app.group.return_to_groups_page()
+    app.session.logout()
