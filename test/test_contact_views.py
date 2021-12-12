1fb022313122c60e67322d9dedf178e6966f593f
natalia <konv.42@gmail.com>
2021-12-13 00:40:12 +0300

    test_contact_views fixed

--- test/test_contact_views.py
+++ test/test_contact_views.py
@@ -3,14 +3,15 @@ from random import randint
 
 
 def test_contact_on_homepage(app):
+    app.contact.check_for_test_contact()
     index = randint(0, (app.contact.count()-1))
     contact_from_homepage = app.contact.get_list()[index]
     contact_from_edit_page = app.contact.get_info_from_edit_page(index)
     assert contact_from_homepage.lastname == contact_from_edit_page.lastname
     assert contact_from_homepage.firstname == contact_from_edit_page.firstname
     assert contact_from_homepage.address == contact_from_edit_page.address
-    assert clear(contact_from_homepage.all_emails) == clear(contact_from_edit_page.all_emails)
-    assert clear(contact_from_homepage.all_phones) == clear(contact_from_edit_page.all_phones)
+    assert clear_newlines(contact_from_homepage.all_emails) == contact_from_edit_page.all_emails
+    assert clear_newlines(contact_from_homepage.all_phones) == clear_phones(contact_from_edit_page.all_phones)
 
 
 #def test_contact_on_detail_page(app):
@@ -28,5 +29,8 @@ def test_contact_on_homepage(app):
 #    assert contact_from_details.home_phone_number2 == contact_from_edit_page.home_phone_number2
 
 
-def clear(s):
+def clear_phones(s):
     return re.sub("[- ()\n]", "", s)
+
+def clear_newlines(s):
+    return re.sub("\n", "", s)
