f142dedf873b8e19424532e27c3419a9eda36777
natalia <konv.42@gmail.com>
2022-01-13 23:59:15 +0300

    steps description for test_add_group in allure report added; screenshot of allure report in jenkins added

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
 
 
 def test_add_group(app, db, check_ui, json_groups):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_list(), key=Group.id_or_max)
+    with pytest.allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step(f'When I add a group {group} to the list'):
+        app.group.create(group)
+    with pytest.allure.step('Then the new group list is equal to the old list with the group added'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_list(), key=Group.id_or_max)
